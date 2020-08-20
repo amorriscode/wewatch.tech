302b308147a71088150eeb292f2c975a3894110c
Anthony Morris <anthony@amorrissound.com>
2020-08-20 08:35:54 -0700

    Add Product Hunt banner

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -11,6 +11,8 @@ const Layout = ({ preview, children }: Props) => {
     <>
       <Meta />
 
+      <a className="fixed top-0 right-0 mr-4 mt-4 z-20" href="https://www.producthunt.com/posts/we-watch-tech?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-we-watch-tech" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=235004&theme=light" alt="We Watch Tech - Great tech talks distilled. | Product Hunt Embed" style={{width: '250px', height: '54px'}} width="250px" height="54px" /></a>
+
       <div>
         <main>{children}</main>
       </div>
